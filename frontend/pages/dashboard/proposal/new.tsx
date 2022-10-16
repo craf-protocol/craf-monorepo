d28e1270f528f2b3610280e8ca2b6fb54a41dc7f
Ben Riazy <hamon.riazy@gmail.com>
2022-10-16 09:16:32 -0500

    loading state

--- frontend/pages/dashboard/proposal/new.tsx
+++ frontend/pages/dashboard/proposal/new.tsx
@@ -40,6 +40,9 @@ function NewProposalForm() {
         const calldata = undefined
         const jsonStringPayload = JSON.stringify(payload);
         setArgs([targets, values, calldata, jsonStringPayload]);
+        if (write) {
+            write()
+        }
     }
 
     return (
@@ -130,8 +133,9 @@ function NewProposalForm() {
                 <button
                     type='submit'
                     className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    disabled={isLoading}
                 >
-                    Submit
+                    {isLoading ? `...Submitting...` : 'Submit'}
                 </button>
             </div>
         </form>
