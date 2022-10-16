b3468a69972da9f0d3754c27bd9a94a5739c1384
Ben Riazy <hamon.riazy@gmail.com>
2022-10-16 08:57:45 -0500

    wip

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,23 +7,43 @@ import {
   connectorsForWallets,
 } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { NavigationBar } from "../components/NavigationBar";
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+
+const prov = jsonRpcProvider({rpc: (chain) => {
+    switch (chain.network) {
+        case "arbitrum-goerli":
+            return {
+                http: `https://solemn-billowing-surf.${chain.network}.discover.quiknode.pro/44ba3f466abe41a34fca70430bc8ff88acbd1929/`,
+                wss: `wss://solemn-billowing-surf.${chain.network}.discover.quiknode.pro/44ba3f466abe41a34fca70430bc8ff88acbd1929/`,
+            }
+        case "homestead":
+            return {
+                http: 'https://eth-mainnet.g.alchemy.com/v2/Yo3FqLiRNG2faWWgn3webF-wi97qURHg',
+                webSocket: 'wss://eth-mainnet.g.alchemy.com/v2/Yo3FqLiRNG2faWWgn3webF-wi97qURHg'
+            }
+        case "arbitrum":
+            return {
+                http: 'https://arb-mainnet.g.alchemy.com/v2/trhh_mk0ukEsZAy03P464_BvYr4UUln6',
+                webSocket: 'wss://arb-mainnet.g.alchemy.com/v2/trhh_mk0ukEsZAy03P464_BvYr4UUln6'
+            }
+    }
+    return {}
+}})
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
+    chain.arbitrumGoerli,
     chain.mainnet,
-    // chain.polygon,
-    // chain.optimism,
     chain.arbitrum,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
       ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
       : []),
   ],
   [
-    alchemyProvider({ apiKey: "_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC" }),
-    publicProvider(),
+      prov,
+      publicProvider(),
   ]
 );
 
