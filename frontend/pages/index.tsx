64297c97c90162de4ef3c5bf3190995390737918
Michael-Vander-Meiden <michaelvandermeiden@gmail.com>
2022-10-16 12:51:22 -0500

    Update index.tsx
    Changed landing page copy

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -24,22 +24,16 @@ export default function LandingPage() {
                 <div className="lg:py-24">
                   <h1 className="mt-4 text-4xl font-bold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
                     <span className="block">A better way to</span>
-                    <span className="block text-indigo-400">fund projects</span>
+                    <span className="block text-indigo-400">fund public goods</span>
                   </h1>
                   <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                    qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                    occaecat fugiat.
+                    CRAF is a dapp that facilitates continuous retroactive public goods funding for projects.
                   </p>
                   <div className="mt-10 sm:mt-12">
                     <form action="#" className="sm:mx-auto sm:max-w-xl lg:mx-0">
                       <p className="mt-3 text-sm text-gray-300 sm:mt-4">
-                        Start your free 14-day trial, no credit card necessary.
-                        By providing your email, you agree to our{" "}
-                        <a href="#" className="font-medium text-white">
-                          terms of service
-                        </a>
-                        .
+                        Donate to the treasury, submit a proposal, and vote on which proposals YOU think deserve funding
+                        for the impact they’ve made on the Arbitrum Ecosystem.
                       </p>
                     </form>
                   </div>
