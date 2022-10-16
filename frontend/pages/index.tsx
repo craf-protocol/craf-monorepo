684988f2c4554417b5b35a7c8ae24492053db4e8
Ben Riazy <hamon.riazy@gmail.com>
2022-10-16 14:14:43 -0500

    fix: footer

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,17 @@
-/*
-  This example requires some changes to your config:
 
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
+function Footer() {
+  return (
+      <footer className="bg-gray-900" aria-labelledby="footer-heading">
+        <div className="mx-auto max-w-7xl py-4 px-4 sm:px-6 lg:py-16 lg:px-8">
+          <div className="border-gray-700 pt-8 md:flex md:items-center md:justify-between">
+            <p className="mt-8 text-base text-gray-400 md:order-1 md:mt-0">
+              &copy; 2022 CRAF, Inc. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
+  )
+}
 
 export default function LandingPage() {
   return (
@@ -55,6 +55,7 @@ export default function LandingPage() {
 
         {/* More main page content here... */}
       </main>
+      <Footer />
     </div>
   );
 }
